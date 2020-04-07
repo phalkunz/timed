0a3100a0e72e5207114e326055896a805dc8b3d8
phalkunz <phalkunz@gmail.com>
2020-04-07 15:57:16 +1200

    Add timer states

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Timer from './components/Timer';
+import { Howl } from 'howler';
 
 function App() {
+
+  const audio = new Howl({
+    src: '/audio/alarm1.m4a',
+    loop: false,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timer seconds={15} onAlarm={() => audio.play()} />
+      <Timer seconds={30} onAlarm={() => audio.play()} />
     </div>
   );
 }
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,40 +1,84 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, FC } from "react";
 
 export type TimerProps = {
   seconds: number;
-  paused?: boolean;
+  onAlarm?: () => void;
 };
 
-const Timer = (props: TimerProps) => {
+const Timer: FC<TimerProps> = (props: TimerProps) => {
   const [ valueInSeconds, setValueInSeconds ] = useState<number>(props.seconds);
+  const [ paused, setPaused ] = useState(false);
+  const [ started, setStarted ] = useState(false);
   const [ intervalId, setIntervalId ] = useState<any>(0);
   const [ delta, setDelta ] = useState<number>(0);
-  const leftInSeconds = Math.round(valueInSeconds - (delta / 1000));
+  const leftInSeconds = Math.abs(Math.round(valueInSeconds - (delta / 1000)));
+
+  const reset = () => {
+    setValueInSeconds(props.seconds);
+    setDelta(0);
+    setStarted(false);
+    setPaused(true);
+  };
+
+
+  React.useMemo(() => {
+    if (leftInSeconds === 0) {
+      props.onAlarm && props.onAlarm();
+      reset();
+    }
+  }, [leftInSeconds, props]);
 
   useEffect(() => {
     const startTime = new Date().getTime();
     setDelta(0);
-    
-    if (props.paused) {
-      console.log('paused');
+
+    if (paused || !started) {
       clearInterval(intervalId);
       setValueInSeconds(leftInSeconds)
     } else {
-      console.log('started');
       setIntervalId(setInterval(() => {
         const now = new Date().getTime();
         setDelta(now - startTime);
-      }, 100));
+        console.log('--- tick!', intervalId);
+      }, 300));
     }
 
     return () => {
       clearInterval(intervalId);
     };
-  }, [props.paused]);
+  }, [started, paused]);
+
+  let toggleButtonLabel = 'Start';
+  if (started) {
+    if (paused) {
+      toggleButtonLabel = 'Resume';
+    } else {
+      toggleButtonLabel = 'Pause'
+    }
+  }
+
+  console.log(toggleButtonLabel, started, paused);
+
+  const handleToggle = () => {
+    if (!started) {
+      setStarted(true);
+      setPaused(false);
+    } else {
+      if (paused) {
+        setPaused(false);
+      } else {
+        setPaused(true);
+      }
+    }
+  }
 
   return (
     <section>
-      <h1>Timer {leftInSeconds < 0 ? 0 : leftInSeconds}</h1>
+      <p>Timer {leftInSeconds < 0 ? 0 : leftInSeconds}</p>
+      <p>
+        <button onClick={() => handleToggle()}>{toggleButtonLabel}</button>
+        <button onClick={() => reset()}>Reset</button>
+      </p>
     </section>
   );
 };
