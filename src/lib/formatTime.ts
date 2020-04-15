01dcfb28e48123c96b9b07d0980253c8007da83f
phalkunz <phalkunz@gmail.com>
2020-04-15 15:22:19 +1200

    formatTime to always have 2 digits

--- src/lib/formatTime.ts
+++ src/lib/formatTime.ts
@@ -1,8 +1,12 @@
+const padLeft = (timePart: number) => {
+  return String(timePart).length >= 2 ? timePart : `0${timePart}`;
+};
+
 const formatTime = (seconds: number): string => {
   const displaySeconds = Math.round(seconds % 60);
   const hours = Math.floor(seconds / (60 * 60));
   const minutes = Math.floor(seconds / 60) - (hours * 60);
-  return `${hours}:${minutes}:${displaySeconds}`;
+  return `${padLeft(hours)}:${padLeft(minutes)}:${padLeft(displaySeconds)}`;
 };
 
 export default formatTime;
